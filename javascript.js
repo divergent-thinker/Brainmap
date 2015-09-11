94775bdedce68cc2e3514e9958035f8786a69dbc
Helen <helen@sigmaforge.com>
2015-09-11 12:45:58 +0100

    added flashing colours to page

--- javascript.js
+++ javascript.js
@@ -14,3 +14,22 @@ function update_current_date () {
 }
 
 window.setInterval(update_current_date, 500);
+
+
+
+var index = 0;
+var classes = ["back_blue", "back_red", "back_green", "back_orange"];
+
+function flashing_colour () {
+
+	document.body.className = classes [index];
+	index = index + 1;
+	if (index >= classes.length) {
+		index = 0;
+	}
+}
+
+window.setInterval(flashing_colour, 500);
+
+
+
